2073ed6c2ddf456364c1ca2d00805ee79a183725
Almir Junior <almirjr944@gmail.com>
2018-08-16 06:40:41 -0300

    38

--- src/main/java/com/almirjr94/curso/boot/web/controller/DepartamentoController.java
+++ src/main/java/com/almirjr94/curso/boot/web/controller/DepartamentoController.java
@@ -1,12 +1,23 @@
 package com.almirjr94.curso.boot.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.almirjr94.curso.boot.web.domain.Departamento;
+import com.almirjr94.curso.boot.web.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
+	
+	@Autowired
+	private DepartamentoService service;
+	
 	
 	@GetMapping("/cadastrar")
 	public String cadastrar() {
@@ -14,8 +25,28 @@ public class DepartamentoController {
 	}
 	
 	@GetMapping("/listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		model.addAttribute("departamentos", service.buscarTodos());
 		return "/departamento/lista";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Departamento departamento) {
+		service.salvar(departamento);
+		return "redirect:/departamentos/cadastrar";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id")Long id, ModelMap model) {
+		model.addAttribute("departamento", service.buscarPorID(id));
+		return "/departamento/cadastro";
+		
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Departamento departamento) {
+		service.editar(departamento);
+		return "redirect:/departamentos/cadastrar";
 	}
 	
 }
